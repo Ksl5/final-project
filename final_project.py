4a54a8e628d3e352996c9ec3722e47a92b3a8a5d
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-25 18:40:49 -0400

    clean up and formatting

--- final_project.py
+++ final_project.py
@@ -10,10 +10,9 @@ load_dotenv()
 
 ### define a function that converts format to one decimal and a percent sign ###
 
-# Info Inputs
-#while 1:
-
 api_key = os.environ.get("FRED_API_KEY")
+
+# User Input and API Pull
 
 try:
     state = input("Please input a state abbreviation: ")
@@ -22,29 +21,18 @@ try:
     response = requests.get(request_url)
 
     parsed_response = json.loads(response.text)
+
     total_observations = parsed_response["count"]
 
-    last_value = float(parsed_response["observations"][total_observations-1]["value"]) # assumes oldest data point comes first, as is FRED standard
 except KeyError:
     print("Hey, didn't find that location. Try again please.")
     exit()
 
+# Getting the state's most recent UR
 
-print("Last Value: " + str(last_value))
+last_value = float(parsed_response["observations"][total_observations-1]["value"]) # assumes oldest data point comes first, as is FRED standard
 
-
-
-# API Pull
-
-
-
-### need an error check here ###
-
-# Getting the most recent value
-
-
-
-# Getting the all-time high/all-time low
+# Getting the state's all-time high/all-time low UR
 
 all_values = []
 index = -1
@@ -57,12 +45,23 @@ for v in parsed_response["observations"]:
 all_time_high = max(all_values)
 all_time_low = min(all_values)
 
-print("All time High: " + str(all_time_high))
-print("All time Low: " + str(all_time_low))
+# Getting the state's pre-COVID-19 unemployment rate
 
-# Getting the pre-COVID-19 state UR
+pre_covid_date = "2020-02-01"
+
+matching_observations = [v for v in parsed_response["observations"] if v["date"] == pre_covid_date] 
+matching_observation = matching_observations[0]
+pre_covid_level = matching_observation["value"]
 
 # Getting the current national UR
 
 # Information Output
 
+print("--------------------------------------------")
+print("The " + str.upper(state) + " Labor Market During COVID-19 Pandemic")
+print("--------------------------------------------")
+print("Current UR: " + ToString(last_value))
+print("February 2020 UR " + str(pre_covid_level))
+print("All Time High UR: " + str(all_time_high))
+print("All Time Low UR: " + str(all_time_low))
+print("--------------------------------------------")
